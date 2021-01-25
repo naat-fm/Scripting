f103a9181a73d0b7458a1599765557a872a36eb9
naat-fm <77945503+naat-fm@users.noreply.github.com>
2021-01-25 12:13:45 -0500

    Add files via upload

---
+++ Triangulo1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Triángulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Inicio
+            Console.WriteLine("Ingrese 'y' :");
+            double y = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese 'z':");
+            double z = double.Parse(Console.ReadLine());
+
+            //Proceso
+      
+            double a = Math.Atan(y / z);
+            double c = Math.Atan(z / y);
+            double t = (3 / Math.Sin(c));
+
+            double agrados = a * (180.0 / Math.PI);
+            double cgrados = c * (180.0 / Math.PI);
+
+            //Resultados 
+            Console.WriteLine("t=" + t);
+            Console.WriteLine("a=" + agrados);
+            Console.WriteLine("c =" + cgrados);
+        }
+    }
+}
