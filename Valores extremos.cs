0a37367c500be24dbeb0df94c7f2b62161725831
naat-fm <77945503+naat-fm@users.noreply.github.com>
2021-02-15 22:04:02 -0500

    Add files via upload

---
+++ Valores extremos.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ValoresExtremos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double total = 0;
+            Console.WriteLine("Ingrese el número de datos");
+            int n = int.Parse(Console.ReadLine());
+            int i = 0, max = 0, min = 500;
+            string nombremax = "";
+            string nombremin = "";
+
+
+            while (i < n)
+            {
+
+                Console.WriteLine("Ingrese nombre, luego la edad");
+                string nombre = Console.ReadLine();
+                int edad = int.Parse(Console.ReadLine());
+
+                if (edad > max)
+                {
+                    max = edad;
+                    nombremax = nombre;
+
+                }
+                if (edad < min)
+                {
+                    min = edad;
+                    nombremin = nombre;
+                }
+
+                total += edad;
+
+                i++;
+            }
+             
+            double promedio = total / n;
+            Console.WriteLine("Promedio : " + promedio);
+            Console.WriteLine("Mayor : " + max + "," + nombremax);
+            Console.WriteLine("Menor : " + min + "," + nombremin);
+
+        }
+    }
+    
+}
