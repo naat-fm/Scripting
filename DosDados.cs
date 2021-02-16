414d5dab170c6d1d4b420f733799a036742819f4
naat-fm <77945503+naat-fm@users.noreply.github.com>
2021-02-15 21:27:02 -0500

    Add files via upload

---
+++ DosDados.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Dos_Dados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random aleatorio = new Random();
+            int dado = 0, total = 0, tiro = 0;
+            string respuesta = "s";
+
+            while (respuesta == "s")
+            {
+                tiro++;
+
+                Console.WriteLine("¿Arrojar dados?(s/n)");
+                respuesta = Console.ReadLine();
+
+                dado = aleatorio.Next(1, 13);
+                Console.WriteLine("Dado= " + dado);
+
+                total += dado;
+
+                Console.WriteLine("Total= " + total);
+                Console.WriteLine("Tiro= " + tiro);
+
+                if (dado == 12 && respuesta == "s")
+                {
+                    Random aleatorionew = new Random();
+                    int dadonew;
+                    dadonew = aleatorionew.Next(1, 13);
+
+                    if (dadonew == 10)
+                    {
+                        Console.WriteLine("Dado=" + dadonew);
+                        Console.WriteLine("Has ganado");
+                        respuesta = "n";
+
+                    }
+                }
+
+                if (tiro > 3)
+                {
+                    Console.WriteLine("No posee más turnos libres");
+
+                    if (dado % 2 != 0)
+                    {
+                        Console.WriteLine("Has perdido");
+                        respuesta = "n";
+                    }
+
+                    if (total > 99)
+                    {
+                        Console.WriteLine("Has ganado");
+                        respuesta = "n";
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ UnDado.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace UnDado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random aleatorio = new Random();
+            string respuesta = "s";
+            int total = 0;
+            int dadoA = 0, dadoB = 0;
+            double contador1 = 0, contador2 = 0;
+            int par = 0, totaltiros = 0;
+
+
+            Console.WriteLine("Desea jugar? (s/n)");
+            respuesta = Console.ReadLine();
+
+            while (respuesta == "s")
+            {
+                contador1 += 1;
+
+                dadoA = aleatorio.Next(1, 7);
+                Console.Write("Dado A:" + dadoA);
+
+                dadoB = aleatorio.Next(1, 7);
+                Console.WriteLine(" Dado B:" + dadoB);
+
+                total = dadoA + dadoB;
+                Console.Write("Total de dados:" + total);
+
+                totaltiros += total;
+                Console.WriteLine(" Total de tiros:" + totaltiros);
+
+                if (total > 6) contador2 += 1;
+
+                if (dadoA == dadoB) par += 1;
+                Console.WriteLine("Total de pares:" + par);
+
+                if ((dadoA == 1) && (dadoB == 1))
+                {
+
+                    Console.WriteLine("Has perdido :(");
+                    respuesta = "n";
+
+                    double porcentaje = (contador2 / contador1) * 100;
+                    Console.WriteLine("Porcentaje de tiros mayores a 6:" + porcentaje);
+
+                }
+
+                if ((totaltiros > 99) || (par == 3))
+                {
+                    Console.WriteLine("Has ganado :)");
+                    respuesta = "n";
+
+                    double porcentaje = (contador2 / contador1) * 100;
+                    Console.WriteLine("Porcentaje de tiros mayores a 6:" + porcentaje);
+                }
+
+                if ((totaltiros < 100) && (respuesta == "s"))
+                {
+                    Console.WriteLine("Desea continuar? (s/n)");
+                    respuesta = Console.ReadLine();
+                }
+
+            }
+        }
+    }
+}
