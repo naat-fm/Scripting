b469b8747c5bb3ddff157db4117d69d8fbb88c00
naat-fm <77945503+naat-fm@users.noreply.github.com>
2021-02-22 19:13:00 -0500

    Add files via upload

---
+++ Seno.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Seno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el valor de X: ");
+
+            double x = double.Parse(Console.ReadLine());
+            Sin(x);
+        }
+
+        static void Sin(double x)
+        {
+            double total = 0;
+            int n = 100;
+
+            for (int i = 0; i < n; i++)
+            {
+                total += ((Math.Pow(-1, i)) / Factorial((2 * i) + 1)) * (Math.Pow(x, (2 * i) + 1));
+            }
+
+            Console.WriteLine("El valor de sin es:" + total);
+
+        }
+
+        static double Factorial(double valor)
+        {
+            if (valor <= 1) return 1;
+            return valor * Factorial(valor - 1);
+        }
+
+
+    }
+}
