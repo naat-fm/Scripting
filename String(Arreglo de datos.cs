95c8b3a82b219a1ba1ef275e364d00e9be564352
naat-fm <77945503+naat-fm@users.noreply.github.com>
2021-03-10 23:36:41 -0500

    Add files via upload

---
+++ String(Arreglo de datos.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace String_Arreglo_de_datos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string frase = "";
+            string palabra = "";
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Ingrese la frase que quieras");
+            Console.ForegroundColor = ConsoleColor.White;
+            frase = Console.ReadLine().ToUpper();
+
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine("Ingrese la palabra que desee encontrar");
+            Console.ForegroundColor = ConsoleColor.White;
+            palabra = Console.ReadLine().ToUpper();
+
+            string[] palabras = frase.Split(" ");
+
+            int contador = 0;
+            for (int i = 0; i < palabras.Length; i++)
+            {
+                if (palabras[i] == palabra)
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine("Palabra en posición:" + i);
+                    Console.ForegroundColor = ConsoleColor.White;
+
+                    contador++;
+                }
+            }
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("Total de veces que apareción la palabra:" + contador);
+            Console.ForegroundColor = ConsoleColor.White;
+
+        }
+    }
+}
